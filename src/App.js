2c6c0f60f1f2183ce61a93839472f5ac35fc8958
5Da <faidhihasif1@gmail.com>
2023-03-08 13:48:09 +0800

    travelWeb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './app.css'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Navbar from './Components/Navbar/Navbar'
+export class App extends Component {
+  render() {
+    return (
+      <>
+      <Navbar/>
+      <Home/>
+      <Main/>
+      <Footer/>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
