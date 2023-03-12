a07aea3a376c4b966d5f90498b25bd10f4e31521
5Da <faidhihasif1@gmail.com>
2023-03-12 21:36:18 +0800

    incomplete

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,155 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './footer.css'
+import video2 from '../../Assets/video/video2.mp4'
+import {FiSend} from 'react-icons/fi'
+import {MdOutlineTravelExplore} from 'react-icons/md'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiFillYoutube} from 'react-icons/ai'
+import {AiFillInstagram} from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
+import {FiChevronRight} from 'react-icons/fi'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Footer = () => {
+  useEffect(() => {
+    Aos.init({duration: 2000})
+  
+  }, [])
+
   return (
-    <div>footer
-        
-    </div>
+    <section className='footer'>
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div data-aos="fade-up" className="text">
+            <small>keep in touch</small>
+            <h2>travel with us</h2>
+
+          </div>
+
+          <div className="inputDiv flex">
+            <input data-aos="fade-up" type="text" name="" id="" placeholder='Enter Email Address' />
+            <button data-aos="fade-up" className="btn flex" type='submit'>
+              SEND<FiSend className="icon"/>
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv flex">
+              <a href="#" className='logo flex'>
+                <MdOutlineTravelExplore className="icon"/> Travel. </a>
+            </div>
+
+            <div data-aos="fade-up" className="footerParagraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+
+            <div data-aos="fade-up" className="footerSocials flex">
+              <AiOutlineTwitter className="icon"/>
+              <AiFillYoutube className="icon"/>
+              <AiFillInstagram className="icon"/>
+              <FaTripadvisor className="icon"/>
+            </div>
+
+          </div>
+
+          <div className="footerLinks grid">
+            <div data-aos="fade-up" data-aos-duration="3000" className="linkGroup">
+              <span className='groupTitle'>
+                our agency
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="4000" className="linkGroup">
+              <span className='groupTitle'>
+                PARTNERS
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Rent Cars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Hostel World
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Trip Advisor
+              </li>
+            </div>
+            
+            <div data-aos="fade-up" data-aos-duration="5000" className="linkGroup">
+              <span className='groupTitle'>
+                LAST MINUTE
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Ocenia
+              </li>
+            </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small>BEST TRAVEL WEBSITE THEME </small>
+            <small>COPYRIGHTS RESERVED - 5D </small>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './main.css'
+import img from '../../Assets/image/image1.jpg'
+import img2 from '../../Assets/image/image2.jpg'
+import img3 from '../../Assets/image/image3.png'
+import {HiOutlineLocationMarker} from 'react-icons/hi'
+import {HiOutlineClipboardCheck} from 'react-icons/hi'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+const Data = [
+  {
+    id:1,
+    imgsrc:img,
+    destTitle: 'Bora Bora',
+    location: "New Zealand",
+    grade: "CULTUTRAL RELAX",
+    fees: "$700",
+    description: "The epitome of romance, Bora Bora is one of the best destinations in the world."
+  },
+  {
+    id: 2,
+    imgsrc:img3,
+    destTitle: 'Cinque Terre',
+    location: "Italy",
+    grade: "CULTUTRAL RELAX",
+    fees: "$840",
+    description: "The vibrant hues of the houses are its benchmark and explain the beauty of this place. It represents the harmonious interaction between people and nature to produce a landscape of exceptional scenic quality"
+  },
+  {
+    id: 3,
+    imgsrc:img2,
+    destTitle: 'Guanajuato',
+    location: "Mexico",
+    grade: "CULTUTRAL RELAX",
+    fees: "$802",
+    description: "Guanajuato is a city in central Mexico. It's known for its silver mining history and colonial architecture."
+  },
+]
 
 const Main = () => {
+  useEffect(() => {
+    Aos.init({duration: 2000})
+  
+  }, [])
+  
+
   return (
-    <div>main
-        
-    </div>
+    <section className='main container section'>
+      <div className="secTitle">
+        <h3 data-aos="fade-right" className="title">
+          Most visited destinations
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+        {
+          Data.map(({id,imgsrc,destTitle,fees,grade,description,location}) => {
+            return(
+              <div key={id} data-aos="fade-up" className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgsrc} alt={destTitle} />
+                </div>
+
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className='icon'/>
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    
+                    <div className="grade">
+                      <span>{grade}<small>+1</small></span>
+                    </div>
+
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+
+                  </div>
+                    <div className="desc">
+                      <p>{description}</p>
+                    </div>
+                  
+                    <button className="btn flex">
+                      DETAILS <HiOutlineClipboardCheck className="icon"/>
+                    </button>
+                </div>
+              </div>
+            )
+          }
+          )
+        }
+      </div>
+
+    </section>
   )
 }
 
